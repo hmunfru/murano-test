64c9f7f9ad083d4ac2c8d6bba51435332980868a
Henar Muñoz Frutos <henar.munozfrutos@telefonica.com>
2015-10-26 13:06:25 +0100

    wait for a contanair

--- start.sh
+++ start.sh
@@ -2,14 +2,7 @@ apt-get install -y curl host syslinux
 host="murano"
 ip="`gethostip -d "$host"`"
 echo $ip
-df -h
-ls -l /opt
 echo "$ip murano.lab.fiware.org" >> /etc/hosts
-cat /etc/hosts
+while ! nc -z murano 8082; do sleep 3; done
 curl http://$ip:8082
-who -m
-ls -l
-cd /test
-ls -l
-echo "hola" >> /test/hola
 sed -i -e "s/XXX/${PASSWORD}/" /etc/tempest/tempest.conf
