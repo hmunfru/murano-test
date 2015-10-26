07d5e367c3c0bb6b6b6b7568ca2fa8ef98db7fdd
Henar Muñoz Frutos <henar.munozfrutos@telefonica.com>
2015-10-26 11:59:40 +0100

    d

--- start.sh
+++ start.sh
@@ -5,4 +5,7 @@ echo $ip
 df -h
 ls -l /opt
 echo "$ip murano.lab.fiware.org" >> /etc/hosts
+cat /etc/hosts
+curl http://$ip:8082
+cat /opt/test/hola
 sed -i -e "s/XXX/${PASSWORD}/" /etc/tempest/tempest.conf
