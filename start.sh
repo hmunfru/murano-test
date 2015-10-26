5afb11ec9098c01942ff7c9f6b02151f4516fc89
Henar Muñoz Frutos <henar.munozfrutos@telefonica.com>
2015-10-26 11:30:08 +0100

    change in start

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 apt-get install -y host syslinux
 host="murano"
 ip="`gethostip -d "$host"`"
+echo $ip
+dh -f
+ls -l /opt/test
 echo "$ip murano.lab.fiware.org" >> /etc/hosts
 sed -i -e "s/XXX/${PASSWORD}/" /etc/tempest/tempest.conf
